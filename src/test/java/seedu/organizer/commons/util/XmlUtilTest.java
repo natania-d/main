3577472b75bcfa2e267983196a9407b9672be91c
Natania Djohari <natania_d@yahoo.com.sg>
2018-04-11 09:46:09 +0800

    Fix checkstyleTest error

--- src/test/java/seedu/organizer/commons/util/XmlUtilTest.java
+++ src/test/java/seedu/organizer/commons/util/XmlUtilTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
