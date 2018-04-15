3b79f420b24192d5c7b8a3ba64f443142e48cef2
Natania Djohari <natania_d@yahoo.com.sg>
2018-04-15 12:09:05 +0800

    Implement RemoveTagsCommand

---
+++ src/main/java/seedu/organizer/logic/parser/RemoveTagsCommandParser.java
@@ -0,0 +1,34 @@
+package seedu.organizer.logic.parser;
+
+import seedu.organizer.commons.exceptions.IllegalValueException;
+import seedu.organizer.logic.commands.RemoveTagsCommand;
+import seedu.organizer.logic.parser.exceptions.ParseException;
+import seedu.organizer.model.tag.Tag;
+
+import java.util.Set;
+
+import static seedu.organizer.logic.parser.CliSyntax.PREFIX_TAG;
+
+/**
+ * Parses input arguments and creates a new RemoveTagsCommand object
+ */
+public class RemoveTagsCommandParser implements Parser<RemoveTagsCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the RemoveTagsCommand
+     * and returns an RemoveTagsCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public RemoveTagsCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_TAG);
+
+        try {
+            Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+            return new RemoveTagsCommand(tagList);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(ive.getMessage(), ive);
+        }
+    }
+}
